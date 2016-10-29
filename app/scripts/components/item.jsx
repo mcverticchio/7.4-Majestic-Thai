c2a7a8c8a583a7ac550ae508394818f62a5afc92
Caroline Verticchio <mcverticchio@gmail.com>
2016-10-29 16:51:57 -0400

    trying to move clicked on items into new array

--- app/scripts/components/item.jsx
+++ app/scripts/components/item.jsx
@@ -5,38 +5,59 @@ require('backbone-react-component');
 
 var OrderCollection = require('../models/item').OrderCollection;
 var menuData = require('../data/menudata').menuItems;
-
+var Checkout = require('./order.jsx').OrderContainer;
 
 var ItemContainer = React.createClass({
-  // mixins: [Backbone.React.Component.mixin],
+  mixins: [Backbone.React.Component.mixin],
+  getInitialState: function(){
+    var orderList = new OrderCollection();
+    return {
+      orderList: orderList
+    }
+  },
+  handleItemClick: function(item){
+    console.log(item)
+    // var orderItem = item.toJSON();
+    delete item.id
+    // //
+    // this.state.orderList.push({item});
+    // this.setState({orderList: this.state.orderList});
+  },
   render: function(){
-    var listofItems = menuData.map((item) =>{
+      var listofItems = menuData.map((item) =>{
 
+      var handleItemClick = this.handleItemClick.bind(this, item);
       var title = item.title;
       var description = item.content;
       var price = item.price;
 
     return(
-      <li key={item.id} className='well'>
+      <li key={item.id} className='well' onClick={handleItemClick}>
         <div>
           <p className="menuTitle">{title}</p>
           <p className="menuDescription">{description}</p>
           <p className="menuPrice">$ {price}0</p>
-          <button type="submit" className="btn btn-primary">Add Item to Cart</button>
         </div>
       </li>
     )
   })
 
   return (
+  <div>
     <ul className="col-md-8">
       {listofItems}
     </ul>
+
+  </div>
   )
 }
 });
 
+// <button onClick={this.handleClick} type="submit" className="btn btn-primary">Add Item to Order</button>
 
+// <div className="col-md-4">
+//   <Checkout orderList={this.state.orderList} />
+// </div>
 module.exports = {
   ItemContainer: ItemContainer
 }
--- app/scripts/components/order.jsx
+++ app/scripts/components/order.jsx
@@ -1,31 +1,50 @@
-var Backbone = require('backbone');
-var React = require('react');
-require('backbone-react-component');
+// var $ = require('jquery');
+// var Backbone = require('backbone');
+// var React = require('react');
+// // var React = require('react-dom');
+// require('backbone-react-component');
+//
+// var OrderCollection = require('../models/item').OrderCollection;
+// // var TemplateContainer = require('./template.jsx').TemplateContainer;
+// var ItemContainer = require('./item.jsx').ItemContainer;
+//
+// var OrderContainer = React.createClass({
+//   mixins:[Backbone.React.Component.mixin],
+//   render: function(){
+    // var orderList = this.props.orderList;
+    // var OrderListing= this.props.orderList.map(function(item){
+//       var title = item.title
+//       var price = item.price;
+//
+//       return(
+//         <div key={item.cid} className="listing">{title}
+//         <div className="checkoutPrice">${price}</div>
+//         </div>
+//       )
+//     });
+//
+//     return(
+//       <div>
+//         <h1>Your Order(s):</h1>
+//         <ul>{Orderlisting}</ul>
+//         <button type="submit" className="btn btn-submit">Submit Order</button>
+//       </div>
+//     )
+//   }
+// });
 
-var OrderCollection = require('./models/item').OrderCollection;
-var TemplateContainer = require('./template.jsx').TemplateContainer;
-var ItemContainer = require('./item.jsx').ItemContainer;
 
-var OrderContainer = React.createClass({
-  render: function(){
-    return(
-
-    )
-  }
-})
-
-
-var SecondScreenContainer = React.createClass({
-  render: function(){
-    return(
-      <TemplateContainer>
-        <ItemContainer />
-        <OrderContainer />
-      </TemplateContainer>
-    )
-  }
-});
+// var SecondScreenContainer = React.createClass({
+//   render: function(){
+//     return(
+//       <TemplateContainer>
+//         <ItemContainer />
+//         <OrderContainer />
+//       </TemplateContainer>
+//     )
+//   }
+// });
 
 // module.exports = {
-//   SecondScreenContainer: SecondScreenContainer
+//   OrderContainer: OrderContainer
 // }
