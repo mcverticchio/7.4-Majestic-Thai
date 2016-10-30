a5a54f204aa54334ca2bfae04c15df854056dfd7
Caroline Verticchio <mcverticchio@gmail.com>
2016-10-30 00:40:04 -0400

    Styling

--- app/scripts/components/template.jsx
+++ app/scripts/components/template.jsx
@@ -3,27 +3,36 @@ var React = require('react');
 var TemplateContainer = React.createClass({
   render: function(){
     return (
-    <div>
-      <div className="container mainHeader">
+      <div className="container">
+        <div className="row mainHeader">
+          <div className="topTapestry"></div>
+            <header className="col-md-12 heading">
+              <h1 className="mainHeading">
+                Majestic Thai
+              </h1>
+              <div className="logo"></div>
+            </header>
+          </div>
+
         <div className="row">
-          <header className="col-md-12 heading">
-            <h1 className="mainHeading">
-              Majestic Thai
-            </h1>
-            <div className="logo"></div>
-          </header>
-          <div className="mainImg"><span>Authentic Thai Food</span></div>
+          <div col-md-12>
+            <div className="mainImg"><span>Authentic Thai Food</span></div>
+          </div>
         </div>
-      </div>
 
         {this.props.children}
 
-          <footer>
-            <h5>Contact Information</h5>
-            <i class="fa fa-envelope" aria-hidden="true"></i><span className="email">majesticthai@email.com</span>
-            <i class="fa fa-phone" aria-hidden="true"></i><span>(888)888-8888</span>
-            <span>1234 Main Street Greenville, SC 00000</span>
-          </footer>
+        <div className="row footer">
+          <div className="col-md-12">
+            <footer>
+              <h5>Contact Information</h5>
+              <i class="fa fa-envelope" aria-hidden="true"></i><span className="email">majesticthai@email.com</span>
+              <i class="fa fa-phone" aria-hidden="true"></i><span>(888)888-8888</span>
+              <span>1234 Main Street Greenville, SC 00000</span>
+            </footer>
+          </div>
+        </div>
+
       </div>
     );
   }
